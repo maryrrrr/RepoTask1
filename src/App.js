87a9134e9ce23b4bc4cd05cbbf5d33ae0fbd8096
maryrrrr <m_rabiee85@yahoo.com>
2023-01-31 00:45:45 +0330

    feat: second edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import MainPage from "./pages/MainPage";
 import ContactUs from "./pages/ContactUs";
 import Inspiration from "./pages/Inspiration";
+import StoryPost from "./pages/StoryPost";
 
 function App() {
   return (
     <>
-     <Inspiration/>
+     <StoryPost/>
     </>
   );
 }
